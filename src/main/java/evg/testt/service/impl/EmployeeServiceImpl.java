a64cf7e74c2683e003bc6874dbbed344fd3cbf53
yananesterenko <ne_yana@mail.ru>
2016-12-01 23:23:22 +0200

    init commit

---
+++ src/main/java/evg/testt/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,11 @@
+package evg.testt.service.impl;
+
+import evg.testt.dao.EmployeeDao;
+import evg.testt.model.Employee;
+import evg.testt.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl extends BaseService<Employee, EmployeeDao> implements EmployeeService {
+
+}
